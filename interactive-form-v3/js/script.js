34d48ffcd4dc86fb73f5b934fd8015bd42e2f20d
aomidfar <82061812+aomidfar@users.noreply.github.com>
2021-10-19 12:29:53 -0700

    first focus, job role, disable color element
    made the first focus to be the name element when the page first loads
    other job role description is hidden unless the other is selected
    color element is disabled by default until the design is selected

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,1 +1,34 @@
 
+// setting the name to be the first focust of the page
+
+const name = document.getElementById('name');
+const nameFocus = name.focus();
+
+//hide the job role text input when the drop down menu "other" has not been selected
+
+const otherJobRole = document.getElementById('other-job-role');
+otherJobRole.style.display="none";
+
+//make the program display the block once "other" is selected AND hide the block if another choice is selected
+const title= document.getElementById('title');
+title.addEventListener('change', (e)=>{
+	if (title.value==='other'){
+
+	otherJobRole.style.display="block";
+	} else {
+		otherJobRole.style.display="none";
+	}
+});
+
+//disable the color element
+const shirtColor = document.getElementById("shirt-colors");
+shirtColor.style.display="none";
+
+//set an event listener to design to detect a change and if so enable shirt color selection
+
+const design = document.getElementById("shirt-designs");
+design.addEventListener('change', e=>{
+
+	const options = document.querySelectorAll("color");
+	shirtColor.style.display='color';
+})
