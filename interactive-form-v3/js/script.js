2e18efc9beb07a7158e1c705334663a0d87a81a0
aomidfar <82061812+aomidfar@users.noreply.github.com>
2021-10-20 11:30:08 -0700

    fixed the theme selection issues
    fixed the theme selection issue as to where it only shows the appropriate available colors for the theme selection by the user

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,4 +1,3 @@
-
 // setting the name to be the first focust of the page
 
 const name = document.getElementById('name');
@@ -21,14 +20,39 @@ title.addEventListener('change', (e)=>{
 });
 
 //disable the color element
-const shirtColor = document.getElementById("shirt-colors");
-shirtColor.style.display="none";
+//const shirtColor = document.getElementById("shirt-colors");
+const color = document.getElementById("color");
+//color.style.display="none";
+
+color.disabled=true;
 
 //set an event listener to design to detect a change and if so enable shirt color selection
 
 const design = document.getElementById("shirt-designs");
 design.addEventListener('change', e=>{
+	console.log(e.target.value);
+	color.disabled=false;
 
-	const options = document.querySelectorAll("color");
-	shirtColor.style.display='color';
+
+		for (let i=0; i < color.children.length; i++){
+
+				if(e.target.value===color.children[i].getAttribute('data-theme')){
+					color.children[i].hidden=false;
+					console.log(color.children[i]);
+					color.children[i].setAttribute('selected', true);
+				}else{
+					color.children[i].hidden=true;
+					color.children[i].removeAttribute('selected');
+					//console.log(color.children[1]);
+				}
+
+		}
+
+		
+		
+
+	
+	
+
 })
+
